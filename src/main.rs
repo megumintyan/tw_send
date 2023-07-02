b7965aca54cd00226ec42695a600d102af43be58
megumintyan <34505936+megumintyan@users.noreply.github.com>
2023-07-02 18:34:38 +0300

    add reply quote poll delete

--- src/main.rs
+++ src/main.rs
@@ -39,6 +39,7 @@ impl Config {
         let client_secret = "";
         let auth_url = "https://twitter.com/i/oauth2/authorize";
         let token_url = "https://api.twitter.com/2/oauth2/token";
+        let redirect_url = "http://localhost";
         let access_token_ttl = 7200;
         let tokens_file_path = "tokens.txt";
 
@@ -47,20 +48,78 @@ impl Config {
             Some(ClientSecret::new(client_secret.to_string())),
             AuthUrl::new(auth_url.to_string()).unwrap(),
             Some(TokenUrl::new(token_url.to_string()).unwrap())
-    ).set_redirect_uri(RedirectUrl::new("http://localhost".to_string()).unwrap());
+    ).set_redirect_uri(RedirectUrl::new(redirect_url.to_string()).unwrap());
         Config { client_id, client_secret, auth_url, token_url, access_token_ttl, tokens_file_path, client }
     }
 }
 
-#[derive(Debug)]
-struct Message<'a> {
-    text: &'a String,
-    id: &'a Option<String>,
+struct Message {
+    message_type: Option<MessageType>,
+    text: Option<String>,
+    id: Option<String>,
+    poll: Option<String>,
+    duration: Option<String>,
+    api: Api,
 }
 
-impl Message <'static>{
-    fn build<'a>(text: &'a String, id: &'a Option<String>) -> Message<'a> {
-        Message { text, id }
+impl Message {
+    fn build(mut args: impl Iterator<Item = String> + ExactSizeIterator) -> Message {
+        args.next();
+        match args.len() {
+            1 => {
+                Message {message_type: Some(MessageType::TEXT),
+                         text: args.next(),
+                         id: None::<String>,
+                         poll: None::<String>,
+                         duration: None::<String>,
+                         api: Api::PostTweets,}
+            }
+            2 => {
+                let message_type = match &args.next().unwrap()[..] {
+                    "-d" => None,
+                    _ => { eprintln!("wrong arument"); std::process::exit(1) }
+                };
+                Message { message_type,
+                          text: None::<String>,
+                          id: args.next(),
+                          poll: None::<String>,
+                          duration: None::<String>,
+                          api: Api::DeleteTweets, }
+            }
+            3 => {
+                let text = args.next();
+                let message_type = match &args.next().unwrap()[..] {
+                    "-r" => Some(MessageType::REPLY),
+                    "-q" => Some(MessageType::QUOTE),
+                    _ => { eprintln!("wrong arument"); std::process::exit(1) }
+                };
+                Message { message_type,
+                          text,
+                          id: args.next(),
+                          poll: None::<String>,
+                          duration: None::<String>,
+                          api: Api::PostTweets}
+
+            }
+            4 => {
+                let text = args.next();
+                let message_type = match &args.next().unwrap()[..] {
+                    "-p" => Some(MessageType::POLL),
+                    _ => { eprintln!("wrong arument"); std::process::exit(1) }
+                };
+                Message { message_type,
+                          text,
+                          id: None::<String>,
+                          poll: args.next(),
+                          duration: args.next(),
+                          api: Api::PostTweets,}
+            }
+            _ => {
+                eprintln!("error: invalid command");
+                std::process::exit(1);
+            }
+        }
+
     }
 
 }
@@ -98,8 +157,7 @@ impl Tokens {
         let refresh_token = token_result.refresh_token().unwrap().secret().clone();
 
         Tokens { timestamp, access_token, refresh_token }
-
- }
+    }
 }
 
 impl Tokens {
@@ -112,38 +170,41 @@ impl Tokens {
         self.timestamp = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs();
         self.access_token = token_result.access_token().secret().clone();
         self.refresh_token = token_result.refresh_token().unwrap().secret().clone();
-
     }
 }
 
-impl<'a> Message <'a>{
-    fn do_tweet(&self, access_token: &str) -> oauth2::http::status::StatusCode {
-        let access_token = format!("Bearer {}", access_token);
-
-        let mut h = oauth2::http::HeaderMap::new();
-        h.insert("Authorization", oauth2::http::HeaderValue::from_str(&access_token).unwrap());
-        h.insert("Content-Type", oauth2::http::HeaderValue::from_str("application/json").unwrap());
-
-        let b: Vec<u8> = match self.id {
-            Some(id) => format!("{{\"text\": \"{}\", \"reply\": {{\"in_reply_to_tweet_id\": \"{}\"}} }}", self.text, id)
-                .as_str()
-                .as_bytes()
-                .to_vec(),
-            None => format!("{{\"text\": \"{}\"}}", self.text)
-                .as_str()
-                .as_bytes()
-                .to_vec(),
+impl Message {
+    fn do_tweet(&self, access_token: &str) -> oauth2::HttpResponse {
+        let body = match self.message_type.as_ref().unwrap() {
+            MessageType::TEXT => format!("{{\"text\": \"{}\"}}", self.text.as_ref().unwrap()),
+            MessageType::REPLY => format!("{{\"text\": \"{}\", \"reply\": {{\"in_reply_to_tweet_id\": \"{}\"}} }}", self.text.as_ref().unwrap(), self.id.as_ref().unwrap()),
+            MessageType::QUOTE => format!("{{\"text\": \"{}\", \"quote_tweet_id\": \"{}\"}}", self.text.as_ref().unwrap(), self.id.as_ref().unwrap()),
+            MessageType::POLL => format!("{{\"text\": \"{}\", \"poll\": {{\"options\": [{}], \"duration_minutes\": {} }}}}", self.text.as_ref().unwrap(), self.poll.as_ref().unwrap(), self.duration.as_ref().unwrap()),
         };
 
-        let r = oauth2::reqwest::http_client(oauth2::HttpRequest {
-            url: oauth2::url::Url::parse("https://api.twitter.com/2/tweets").unwrap(),
-            method: oauth2::http::Method::POST,
-            headers: h,
-            body: b})
-        .unwrap();
-
-        r.status_code
+        let url = "https://api.twitter.com/2/tweets".to_string();
+        request(url, oauth2::http::Method::POST, body, &access_token)
     }
+
+    fn delete(&self, access_token: &str) -> oauth2::HttpResponse {
+        let url = format!("{}/{}", "https://api.twitter.com/2/tweets", self.id.as_ref().unwrap());
+        request(url, oauth2::http::Method::DELETE, "".to_string(), &access_token)
+    }
+}
+
+fn request (url: String, method: oauth2::http::Method, body: String, access_token: &str) -> oauth2::HttpResponse {
+    let access_token = format!("Bearer {}", access_token);
+    let mut headers = oauth2::http::HeaderMap::new();
+    headers.insert("Authorization", oauth2::http::HeaderValue::from_str(&access_token).unwrap());
+    headers.insert("Content-Type", oauth2::http::HeaderValue::from_str("application/json").unwrap());
+
+    let r = oauth2::reqwest::http_client(oauth2::HttpRequest {
+        url: oauth2::url::Url::parse(url.as_str()).unwrap(),
+        method,
+        headers,
+        body: body.into()})
+        .unwrap();
+    r
 }
 
 #[derive(Debug)]
@@ -197,23 +258,27 @@ impl Tokens {
     }
 }
 
+enum MessageType {
+    TEXT,
+    REPLY,
+    QUOTE,
+    POLL,
+}
+
+enum Api {
+    PostTweets,
+    DeleteTweets,
+}
+
 fn main() {
     let config = Config::get_config();
     let tokens = Tokens::get_tokens(&config);
 
-    let mut args = env::args();
-    if args.len() < 2 || args.len() > 3 {
-        eprintln!("usage: {} 'some text' {{optional id}}", args.next().unwrap());
-        std::process::exit(1);
-    }
+    let message = Message::build(env::args());
+    let response = match message.api {
+        Api::PostTweets => message.do_tweet(&tokens.access_token),
+        Api::DeleteTweets => message.delete(&tokens.access_token),
+    };
 
-    args.next();
-
-    let text = args.next().unwrap();
-    let id = args.next();
-
-    let status_code = Message::build(&text, &id)
-        .do_tweet(&tokens.access_token);
-    println!("{}", status_code.to_string());
-
+    println!("{} → {}", response.status_code.to_string(), String::from_utf8(response.body).unwrap());
 }
